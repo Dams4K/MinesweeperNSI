c6ab164f306be1df1e29526195d0a7769eeba989
Linghuun <96080056+Linghuun@users.noreply.github.com>
2023-03-07 13:45:11 +0100

    presque finit
    manque : 
    - Fonction quand on perd
    - Fonction quand on gagne
    - problème de pourcentage dont tu as parlé ce matin
    - texture carré vide découvert

--- minesweep.py
+++ minesweep.py
@@ -2,17 +2,45 @@ import tkinter as tk
 from random import randint
 
 
-def afficher(x,y):
-    nb_mines = voisin(x,y)
-    minesgrid[(x,y)].config(text = f"{nb_mines}")
+def discover(event):
+    button = event.widget
+    if button["image"] != 'pyimage1':
+        for key, value in minesgrid.items():
+            if value == button:
+                x, y = key
+    voisins = voisin(x,y)
+    nb_mines = sum([minesweep[x1][y1] for x1,y1 in voisins])
+    nb_drapeau = sum([1 for x1,y1 in voisins if minesgrid[(x1,y1)]["image"] == 'pyimage1'])
+    if nb_mines == nb_drapeau:
+        for x1,y1 in voisins:
+            afficher_nb_mines(x1, y1)
+
+        
+
+def perdu():
+    print("perdu")
+
+def afficher_nb_mines(x,y):
+    if minesgrid[(x,y)]['image'] != 'pyimage1':
+        voisins = voisin(x,y)
+        nb_mines = sum([minesweep[x1][y1] for x1,y1 in voisins])
+        if nb_mines !=0:
+            minesgrid[(x,y)] = tk.Button(root, image = pixel, text = nb_mines, justify="center", compound="c")
+        else:
+            minesgrid[(x,y)] = tk.Button(root, image = pixel, justify="center", compound="c")
+        minesgrid[(x,y)].grid(row = x, column = y)
+        minesgrid[(x,y)].bind("<Button-2>", discover)
 
 def on_button_click(event):
     button = event.widget
-    for key, value in minesgrid.items():
-        if value == button:
-            x, y = key
-            afficher(x, y)
-            break
+    if button["image"] != 'pyimage1':
+        for key, value in minesgrid.items():
+            if value == button:
+                x, y = key
+                if minesweep[x][y] == 1:
+                    perdu()
+                afficher_nb_mines(x, y)
+                break
 
 def voisin(x,y):
     global w,h
@@ -21,9 +49,9 @@ def voisin(x,y):
     for i in liste:
         for j in liste:
             if 0<= x+i <= h-1 and 0<= y+j <= w-1:
-                voisins.append(minesweep[x+i][y+j])
+                voisins.append((x+i, y+j))
 
-    return sum(voisins)
+    return voisins
             
 def flag(event):
     button = event.widget
@@ -31,6 +59,8 @@ def flag(event):
         button.config(image='pyimage2')   
     else:
         button.config(image='pyimage1')
+        
+
 
 
 root = tk.Tk()
@@ -38,8 +68,7 @@ root = tk.Tk()
 w = 9
 h = 10
 
-n = 100
-debug = 1
+n = 20
 
 Drapeau = tk.PhotoImage(master=root, file ='Drapeau.png') # 'pyimage1'
 pixel = tk.PhotoImage(width=50, height=50, master=root)   # 'pyimage2'
@@ -50,10 +79,10 @@ for line in minesweep:
     for elt in range(len(line)):
         if randint(0,100) <= n:
             line[elt] = 1
-def DEBUG():
-    if debug:
-        for line in minesweep:
-            print(line)
+
+for line in minesweep:
+    print(line)
+
 
 minesgrid = {}
 for line in range(len(minesweep)):
@@ -63,8 +92,9 @@ for line in range(len(minesweep)):
 
 
 for button in minesgrid.values():
-    button.bind("<Button-1>", on_button_click)
-    button.bind("<Button-3>", flag)
+    if button['text'] == "":
+        button.bind("<Button-1>", on_button_click)
+        button.bind("<Button-3>", flag)
 
 
 root.mainloop()
