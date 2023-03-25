3c543e6e05c547a4edd407c4846f0355f27976f7
Dams4K <dams4k@users.noreply.github.com>
2023-03-25 10:52:04 +0100

    good cameracontroller

--- Godot 3/minesweeper/CameraController.gd
+++ Godot 3/minesweeper/CameraController.gd
@@ -14,16 +14,14 @@ func _ready():
 
 func _input(event):
 	if Input.is_action_just_released("zoom_in"):
-		var before_mouse_pos: Vector2 = camera2D.get_local_mouse_position()
+		var mouse_pos: Vector2 = camera2D.get_local_mouse_position()
+		camera2D.position += mouse_pos - mouse_pos / (1+ZOOM_SPEED)
 		camera2D.zoom /= 1+ZOOM_SPEED
-		var after_mouse_pos: Vector2 = camera2D.get_local_mouse_position()
-		camera2D.position -= after_mouse_pos - before_mouse_pos
 		
 	if Input.is_action_just_released("zoom_out"):
-		var before_mouse_pos: Vector2 = camera2D.get_local_mouse_position()
+		var mouse_pos: Vector2 = camera2D.get_local_mouse_position()
+		camera2D.position += mouse_pos - mouse_pos / (1-ZOOM_SPEED)
 		camera2D.zoom /= 1-ZOOM_SPEED
-		var after_mouse_pos: Vector2 = camera2D.get_local_mouse_position()
-		camera2D.position -= after_mouse_pos - before_mouse_pos
 	
 	if Input.is_action_pressed("move_camera") and event is InputEventMouseMotion:
 		camera2D.position += event.relative * -1 * camera2D.zoom
