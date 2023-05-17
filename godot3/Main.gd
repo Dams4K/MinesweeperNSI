bb726ec5e3e0573f83b4875bbc25fd1770feaf19
dams4k <dams4k@users.noreply.github.com>
2023-05-17 10:23:51 +0200

    zoom the camera when a game is started for the entire map to be seen

--- godot3/Main.gd
+++ godot3/Main.gd
@@ -7,7 +7,14 @@ onready var lose_menu = $CanvasLayer/LoseMenu
 func _ready():
 	Minesweeper.clear()
 	lose_menu.hide()
+	
 	self.camera2D.position = Minesweeper.size * 64 / 2
+	
+	var viewport_rect = get_viewport_rect()
+	var width_zoom = (Minesweeper.size.x + 2) * 64 / viewport_rect.size.x
+	var height_zoom = (Minesweeper.size.y + 2) * 64 / viewport_rect.size.y
+	self.camera2D.zoom = Vector2.ONE * max(width_zoom, height_zoom)
+	
 	self.minesweeperGui.generate()
 
 
