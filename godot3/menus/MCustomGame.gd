57d452855cac304ca28cb39c5e89304b08f8963c
dams4k <dams4k@users.noreply.github.com>
2023-05-20 18:26:05 +0200

    grass background

--- godot3/menus/MCustomGame.gd
+++ godot3/menus/MCustomGame.gd
@@ -10,7 +10,10 @@ onready var width_spin_box = $Panel/VBoxContainer/WidthContainer/WidthSpinBox
 
 onready var height_spin_box = $Panel/VBoxContainer/HeightContainer/HeightSpinBox
 
+onready var grass_tile_map = $GrassTileMap
+
 func _ready():
+	_on_Control_resized()
 	mines_slider.share(mines_percentage)
 
 
@@ -18,3 +21,13 @@ func _on_ValidateButton_pressed():
 	Minesweeper.size = Vector2(clamp(width_spin_box.value, 2, 100), clamp(height_spin_box.value, 2, 100))
 	Minesweeper.bombs_percentage = mines_percentage.value
 	get_tree().change_scene(GAME)
+
+
+func _on_Control_resized():
+	if not grass_tile_map:
+		return
+	
+	for y in range(int(get_viewport_rect().size.y / grass_tile_map.cell_size.y) + 1):
+		for x in range(int(get_viewport_rect().size.x / grass_tile_map.cell_size.x) + 1):
+			if grass_tile_map.get_cell(x, y) == -1:
+				grass_tile_map.set_grass(x, y)
--- godot3/menus/MSelectGame.gd
+++ godot3/menus/MSelectGame.gd
@@ -6,6 +6,8 @@ const M_CUSTOM_GAME = "res://menus/MCustomGame.tscn"
 export var default_minesweeper_size = Vector2.ONE * 9
 export var default_minesweeper_percentage = 0.15
 
+onready var grass_tile_map = $GrassTileMap
+
 func _ready():
 	Minesweeper.size = default_minesweeper_size
 	Minesweeper.bombs_percentage = default_minesweeper_percentage
@@ -24,3 +26,13 @@ func _on_button24x24_pressed():
 
 func _on_buttoncustom_pressed():
 	get_tree().change_scene(M_CUSTOM_GAME)
+
+
+func _on_Control_resized():
+	if not grass_tile_map:
+		return
+	
+	for y in range(int(get_viewport_rect().size.y / grass_tile_map.cell_size.y) + 1):
+		for x in range(int(get_viewport_rect().size.x / grass_tile_map.cell_size.x) + 1):
+			if grass_tile_map.get_cell(x, y) == -1:
+				grass_tile_map.set_grass(x, y)
