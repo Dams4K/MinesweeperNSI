fa5f06b4c99c1f95afcf6a120d1addee36784b3b
dams4k <dams4k@users.noreply.github.com>
2023-05-20 14:08:40 +0200

    max zoom / min zoom

--- godot3/minesweeper/gui/CameraController.gd
+++ godot3/minesweeper/gui/CameraController.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 const ZOOM_SPEED = 0.1
 
@@ -6,22 +6,41 @@ export var camera_path: NodePath
 
 var camera2D: Camera2D
 
-var original_pos: Vector2 = Vector2.ZERO
+var max_zoom = 100
+var min_zoom = 0.125
 
 func _ready():
 	camera2D = get_node_or_null(camera_path)
 	camera2D.current = true
+	
+	camera2D.position = Minesweeper.size * 64 / 2
+	
+	var viewport_rect = get_viewport_rect()
+	
+	var width_zoom = (Minesweeper.size.x + 2) * 64 / viewport_rect.size.x
+	var height_zoom = (Minesweeper.size.y + 2) * 64 / viewport_rect.size.y
+	camera2D.zoom = Vector2.ONE * max(width_zoom, height_zoom)
+	max_zoom = max(width_zoom, height_zoom) * 2
 
 func _input(event):
 	if Input.is_action_just_released("zoom_in"):
 		var mouse_pos: Vector2 = camera2D.get_local_mouse_position()
-		camera2D.position += mouse_pos - mouse_pos / (1+ZOOM_SPEED)
-		camera2D.zoom /= 1+ZOOM_SPEED
+		if set_camera_zoom(1+ZOOM_SPEED):
+			camera2D.position += mouse_pos - mouse_pos / (1+ZOOM_SPEED)
 		
 	if Input.is_action_just_released("zoom_out"):
 		var mouse_pos: Vector2 = camera2D.get_local_mouse_position()
-		camera2D.position += mouse_pos - mouse_pos / (1-ZOOM_SPEED)
-		camera2D.zoom /= 1-ZOOM_SPEED
+		if set_camera_zoom(1-ZOOM_SPEED):
+			camera2D.position += mouse_pos - mouse_pos / (1-ZOOM_SPEED)
 	
 	if Input.is_action_pressed("move_camera") and event is InputEventMouseMotion:
 		camera2D.position += event.relative * -1 * camera2D.zoom
+
+func set_camera_zoom(new_zoom):
+	var value = clamp(camera2D.zoom.x / new_zoom, min_zoom, max_zoom)
+	print(value)
+	if stepify(camera2D.zoom.x, 0.02) != stepify(value, 0.02):
+		camera2D.zoom = Vector2.ONE * value
+		print(camera2D.zoom)
+		return true
+	return false
--- godot3/minesweeper/gui/Game.gd
+++ godot3/minesweeper/gui/Game.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-onready var camera2D = $Camera2D
 onready var minesweeperGui = $MinesweeperGui
 
 onready var canvas_layer = $CanvasLayer
@@ -11,13 +10,6 @@ func _ready():
 	Minesweeper.clear()
 	lose_menu.hide()
 	win_menu.hide()
-	
-	self.camera2D.position = Minesweeper.size * 64 / 2
-	
-	var viewport_rect = get_viewport_rect()
-	var width_zoom = (Minesweeper.size.x + 2) * 64 / viewport_rect.size.x
-	var height_zoom = (Minesweeper.size.y + 2) * 64 / viewport_rect.size.y
-	self.camera2D.zoom = Vector2.ONE * max(width_zoom, height_zoom)
 	
 	self.minesweeperGui.generate()
 
