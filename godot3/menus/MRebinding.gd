a538c8444f57fb23e9715a9ebc6d2109c14bc5a1
dams4k <dams4k@users.noreply.github.com>
2023-05-21 12:57:06 +0200

    Rebinding final

--- godot3/menus/MRebinding.gd
+++ godot3/menus/MRebinding.gd
@@ -14,6 +14,7 @@ var is_listening = false
 
 onready var label = $"%Label"
 onready var button = $"%Button"
+onready var timer = $"%Timer"
 
 func _ready():
 	label.text = action_name
@@ -27,19 +28,23 @@ func _process(delta):
 func _input(event):
 	if not is_listening:
 		return
+	if not event is InputEventKey and not event is InputEventMouseButton:
+		return
 	
 	if event is InputEventKey:
 		is_listening = false
 		InputMap.action_erase_events(action)
 		InputMap.action_add_event(action, event)
-		Config.save_input_map()
 	elif event is InputEventMouseButton:
 		is_listening = false
 		var index = event.button_index
 		if MOUSE_NAMES.has(index):
 			InputMap.action_erase_events(action)
 			InputMap.action_add_event(action, event)
-		Config.save_input_map()
+	
+	button.disabled = true
+	timer.start()
+	Config.save_input_map()
 
 func update_button():
 	var actions = InputMap.get_action_list(action)
@@ -59,3 +64,7 @@ func set_action_name(v):
 
 func _on_Button_pressed():
 	is_listening = not is_listening
+
+
+func _on_Timer_timeout():
+	button.disabled = false
