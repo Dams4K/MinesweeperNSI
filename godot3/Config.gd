16738b53ddfaca25293ce28a758215eb03d9ba48
dams4k <dams4k@users.noreply.github.com>
2023-05-21 12:40:52 +0200

    Rebinding

--- godot3/Config.gd
+++ godot3/Config.gd
@@ -5,11 +5,32 @@ const CONFIG_PATH = "user://config.cfg"
 var config := ConfigFile.new()
 
 func _ready():
-	config.load(CONFIG_PATH)
+	var err = config.load(CONFIG_PATH)
+	if err == OK:
+		for action in InputMap.get_actions():
+			var events = InputMap.get_action_list(action)
+			if not events:
+				continue
+			
+			var custom_event = config.get_value("Inputs", action, false)
+			if custom_event is bool: # no custom event
+				continue
+			
+			InputMap.action_erase_events(action)
+			InputMap.action_add_event(action, custom_event)
 
 func get_value(section, key, default_value):
 	return config.get_value(section, key, default_value)
 
 func set_value(section, key, value):
 	config.set_value(section, key, value)
+	config.save(CONFIG_PATH)
+
+func save_input_map():
+	for action in InputMap.get_actions():
+		var events = InputMap.get_action_list(action)
+		if not events:
+			continue
+		
+		config.set_value("Inputs", action, events[0])
 	config.save(CONFIG_PATH)
--- godot3/menus/MRebinding.gd
+++ godot3/menus/MRebinding.gd
@@ -1,8 +1,16 @@
 tool
 extends HBoxContainer
 
+const MOUSE_NAMES = {
+	BUTTON_LEFT: "Mouse Left",
+	BUTTON_RIGHT: "Mouse Right",
+	BUTTON_MIDDLE: "Mouse Middle",
+}
+
 export var action: String
 export var action_name: String setget set_action_name
+
+var is_listening = false
 
 onready var label = $"%Label"
 onready var button = $"%Button"
@@ -10,7 +18,44 @@ onready var button = $"%Button"
 func _ready():
 	label.text = action_name
 
+func _process(delta):
+	if is_listening:
+		button.text = "Appuyez sur une touche"
+	else:
+		update_button()
+
+func _input(event):
+	if not is_listening:
+		return
+	
+	if event is InputEventKey:
+		is_listening = false
+		InputMap.action_erase_events(action)
+		InputMap.action_add_event(action, event)
+		Config.save_input_map()
+	elif event is InputEventMouseButton:
+		is_listening = false
+		var index = event.button_index
+		if MOUSE_NAMES.has(index):
+			InputMap.action_erase_events(action)
+			InputMap.action_add_event(action, event)
+		Config.save_input_map()
+
+func update_button():
+	var actions = InputMap.get_action_list(action)
+	if actions:
+		var action = actions[0]
+		if action is InputEventKey:
+			button.text = OS.get_scancode_string(action.scancode)
+		elif action is InputEventMouseButton:
+			var index = action.button_index
+			if MOUSE_NAMES.has(index):
+				button.text = MOUSE_NAMES[index]
+
 func set_action_name(v):
 	action_name = v
 	if label:
 		label.text = action_name
+
+func _on_Button_pressed():
+	is_listening = not is_listening
